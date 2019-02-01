206b9aabf91c3893c50ba65126736a049c838b3f
evanscho <evan.schoenbach@gmail.com>
2019-02-01 16:55:55 -0200

    add click listener to remove error messages

--- lists/static/list.js
+++ lists/static/list.js
@@ -3,4 +3,7 @@ window.Superlists.initialize = function () {
   $('input[name="text"]').on('keyup', function () {
     $('.has-error').hide();
   });
+  $('input[name="text"]').on('click', function () {
+    $('.has-error').hide();
+  });
 };
